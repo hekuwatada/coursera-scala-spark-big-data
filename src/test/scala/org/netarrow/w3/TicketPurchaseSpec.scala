2a838dc08f1b1a819b33f8124adb7d5c289e1e52
Heku Watada <heku.watada@gmail.com>
2019-03-17 10:54:24 +0000

    Unified two calculations per visitor in one.

--- src/test/scala/org/netarrow/w3/TicketPurchaseSpec.scala
+++ src/test/scala/org/netarrow/w3/TicketPurchaseSpec.scala
@@ -17,26 +17,15 @@ class TicketPurchaseSpec extends FunSpec with Matchers with SparkLocal {
       TicketPurchase(3, "destC", 175),
       TicketPurchase(3, "destD", 120)
     )
-
-    it("calculates how many trips were made by each visitor") {
-      withSparkContext { sc =>
-        val purchasesRdd: RDD[TicketPurchase] = sc.parallelize(purchases)
-
-        val destPairRdd: RDD[(Int, String)] = purchasesRdd.map(p => (p.customerId, p.destination))
-        val groupedDest: RDD[(Int, Iterable[String])] = destPairRdd.groupByKey()
-        val countDest: RDD[(Int, Int)] = groupedDest.mapValues(d => d.toSeq.size)
-        countDest.collect() should contain theSameElementsAs Map(1 -> 3, 2 -> 2, 3 -> 2)
-      }
-    }
-
-    it("calculates how much money was spent by each visitor") {
+    
+    it("calculates how many trips were made and how much money was spent by each visitor") {
       withSparkContext { sc =>
         val purchasesRdd: RDD[TicketPurchase] = sc.parallelize(purchases)
 
         val pricePairRdd: RDD[(Int, Double)] = purchasesRdd.map(p => (p.customerId, p.price))
         val groupedPrice: RDD[(Int, Iterable[Double])] = pricePairRdd.groupByKey()
-        val totalPrice: RDD[(Int, Double)] = groupedPrice.mapValues(p => p.sum)
-        totalPrice.collect() should contain theSameElementsAs Map(1 -> 200, 2 -> 220, 3 -> 295)
+        val tripsCost: RDD[(Int, (Int, Double))] = groupedPrice.mapValues(p => (p.size, p.sum))
+        tripsCost.collect() should contain theSameElementsAs Map(1 -> (3, 200), 2 -> (2, 220), 3 -> (2, 295))
       }
     }
 
